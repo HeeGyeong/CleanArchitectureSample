af979b6ef93a44cfeb6bf2675d5d849cbac8f28c
HeeGyeong <fhskf94kr@naver.com>
2022-02-28 20:43:19 +0900

    Espresso 적용

--- CleanArchitectureStudy/presentaion/src/androidTest/java/com/example/cleanarchitecturestudy/ExampleInstrumentedTest.kt
+++ CleanArchitectureStudy/presentaion/src/androidTest/java/com/example/cleanarchitecturestudy/ExampleInstrumentedTest.kt
@@ -1,24 +1,45 @@
 package com.example.cleanarchitecturestudy
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.rule.ActivityTestRule
+import com.example.cleanarchitecturestudy.view.MainActivity
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.cleanarchitecturestudy", appContext.packageName)
+    fun test_espresso() {
+        Espresso.onView(withId(R.id.thirdBtn))
+            .perform(ViewActions.click())
+        Espresso.onView(withId(R.id.et_input))
+            .perform(ViewActions.typeText("call"), ViewActions.closeSoftKeyboard())
+        Espresso.onView(withId(R.id.et_input))
+            .check(matches(withText("call")))
+        Espresso.onView(withId(R.id.btn_search))
+            .perform(ViewActions.click())
+        CountDownLatch(1).await(5, TimeUnit.SECONDS) // Delay
+//        Thread.sleep(5000L)
+        Espresso.pressBack()
+        CountDownLatch(1).await(3, TimeUnit.SECONDS)
+        Espresso.onView(withId(R.id.thirdBtn))
+            .perform(ViewActions.click())
+        Espresso.onView(withId(R.id.et_input))
+            .perform(ViewActions.typeText("BED"), ViewActions.closeSoftKeyboard())
+        Espresso.onView(withId(R.id.btn_search))
+            .perform(ViewActions.click())
+        CountDownLatch(1).await(5, TimeUnit.SECONDS)
     }
 }
