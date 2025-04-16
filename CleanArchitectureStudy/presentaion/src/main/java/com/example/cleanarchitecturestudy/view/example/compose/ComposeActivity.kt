9fa1132d80d84a0f49ee577664907925f6fef5d7
HeeGyeong <fhskf94kr@naver.com>
2025-04-16 21:13:39 +0900

    feat: Compose UI 내부에 XML 추가

--- CleanArchitectureStudy/presentaion/src/main/java/com/example/cleanarchitecturestudy/view/example/compose/ComposeActivity.kt
+++ CleanArchitectureStudy/presentaion/src/main/java/com/example/cleanarchitecturestudy/view/example/compose/ComposeActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import com.example.cleanarchitecturestudy.R
@@ -31,7 +40,7 @@ class ComposeActivity :
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(300.dp)
+                    .wrapContentHeight()
             ) {
                 ComposeTextComponent("Hello Compose")
 
@@ -42,6 +51,32 @@ class ComposeActivity :
 
                     ComposeTextComponent("Hello Compose")
                 }
+
+                Divider(
+                    color = Color.Black,
+                    thickness = 1.dp,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(max = 300.dp)
+                        .background(color = Color.Yellow)
+                ) {
+                    items(100) {
+                        ComposeTextComponent("Hello Compose")
+                    }
+                }
+
+                Divider(
+                    color = Color.Black,
+                    thickness = 4.dp,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                )
+
+                // AndroidView를 사용하여 XML 레이아웃 통합
+                XmlInComposeExample()
             }
         }
     }
--- CleanArchitectureStudy/presentaion/src/main/java/com/example/cleanarchitecturestudy/view/example/compose/ComposeComponent.kt
+++ CleanArchitectureStudy/presentaion/src/main/java/com/example/cleanarchitecturestudy/view/example/compose/ComposeComponent.kt
@@ -1,11 +1,36 @@
 package com.example.cleanarchitecturestudy.view.example.compose
 
+import android.view.LayoutInflater
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.cleanarchitecturestudy.R
 
 @Composable
 fun ComposeTextComponent(text: String) {
     Text(
         text = text
+    )
+}
+
+
+@Composable
+fun XmlInComposeExample() {
+    // AndroidView를 사용하여 XML 레이아웃을 Compose에 통합
+    AndroidView(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        factory = { ctx ->
+            val inflater = LayoutInflater.from(ctx)
+            val view = inflater.inflate(R.layout.activity_main, null, false)
+            view.tag = "activity_main"
+
+            view
+        }
     )
 }
